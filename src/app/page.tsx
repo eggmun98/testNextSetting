6253e54495045f05b0d8fe2dbf9a9850e33c8389
eggmun98 <eggmun98@gmail.com>
2024-07-28 18:52:02 +0900

    ai리뷰 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,24 @@ export default function Home() {
     <main>
       <div className={container}>test</div>
       <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <div className={container}>난 누굴까</div>
+      <li>
+        <ol>1</ol>
+        <ol>2</ol>
+        <ol>3</ol>
+      </li>
+      <li>
+        <ol>1</ol>
+        <ol>2</ol>
+        <ol>3</ol>
+      </li>
       <li>
         <ol>1</ol>
         <ol>2</ol>
