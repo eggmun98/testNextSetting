5244d2a50cbb513a0e1d841b39b7c188f3224a89
eggmun98 <eggmun98@gmail.com>
2024-07-28 18:51:22 +0900

    깃허브 액션 워크플로우 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import { container } from "./example.css";
+import { container } from './example.css';
 
 export default function Home() {
   return (
     <main>
       <div className={container}>test</div>
+      <div className={container}>난 누굴까</div>
+      <li>
+        <ol>1</ol>
+        <ol>2</ol>
+        <ol>3</ol>
+      </li>
     </main>
   );
 }
