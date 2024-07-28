008f7794b150992ede508bd4c5041c9418a2c3ba
eggmun98 <eggmun98@gmail.com>
2024-07-28 19:00:18 +0900

    ai리뷰 테스트2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { container } from './example.css';
 
 export default function Home() {
+  const [qqq, setQqq] = useState();
+
+  useEffect(() => setQqq(1), []);
+
   return (
     <main>
       <div className={container}>test</div>
